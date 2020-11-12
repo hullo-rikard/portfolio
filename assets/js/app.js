62905c37b95f2f5547ff46f6c855a6cf78743200
rikardbjerkenius <rikard@hullo.se>
2020-11-12 10:18:46 +0100

    fixed navigation, styling added info nackademin

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,21 @@
-//JQuery Module Pattern
-
-// An object literal
-var app = {
-  init: function() {
-    app.functionOne();
-  },
-  functionOne: function () {
-  }
-};
 $("document").ready(function () {
-  app.init();
+
+  $(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top -65
+    }, 500);
+});
+
+  $(window).scroll( function(){
+    $('.fade-in-section').each( function(i){
+        var top_of_object = $(this).position().top + 50;
+        var bottom_of_window = $(window).scrollTop() + $(window).height();
+        if( bottom_of_window > top_of_object ){
+            $(this).addClass('is-visible')
+        }
+    }); 
+  });
+
 });
