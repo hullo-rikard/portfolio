0d4b65b879572bae629fe0d9beeb6e28ccde55c9
rikardbjerkenius <rikard@hullo.se>
2020-11-12 13:11:03 +0100

    without css preload

--- gulpfile.js
+++ gulpfile.js
@@ -100,10 +100,7 @@ gulp.task('replaceHtmlBlock', function () {
   return gulp.src(['*.html'])
     .pipe(htmlreplace({
       'js': 'assets/js/app.min.js',
-      'css': {
-        src: [['assets/css/app.min.css', 'assets/css/app.min.css']],
-        tpl: '<link rel="preload" href="%s" as="style" onload="this.onload=null;this.rel=\'stylesheet\'"><noscript><link rel="stylesheet" href="%s"></noscript>'
-      }
+      'css': 'assets/css/app.min.css'
     }))
     .pipe(gulp.dest('dist/'));
 });
